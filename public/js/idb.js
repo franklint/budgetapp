5ad522aeb92a82e4e08c7ca89b8d33c233b7fa84
franklinT <franklintsung3@gmail.com>
2022-07-13 17:56:36 -0400

    sixth

--- public/js/idb.js
+++ public/js/idb.js
@@ -16,42 +16,41 @@ request.onupgradeneeded = function(event) {
 // upon a successful
 request.onsuccess = function(event) {
 
-    // when db is successfully created with its object store or simply established a connection, save reference to db in global variable
+    
     db = event.target.result;
 
-    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
     if (navigator.onLine) {
-        // todo: uploadTransaction();
+
     }
 };
 
 request.onerror = function(event) {
-    // log error here
+ 
     console.log(event.target.errorCode);
 };
 
-// This function will be executed if we attempt to submit a new transaction and there's no internet connection
+
 function saveRecord(record) {
 
-    // open a new transaction with the database with read and write permissions
+    
     const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-    // access the object store for `new_transaction`
+
     const  budgetObjectStore = transaction.objectStore('new_transaction');
 
-    // add record to your store with add method
+    
     budgetObjectStore.add(record);
 }
 
 function uploadTransaction() {
 
-    // open a transaction on your db
+   
     const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-    // access your object store
+    
     const budgetObjectStore = transaction.objectStore('new_transaction');
 
-    // get all records from store and set to a variable
+    
     const getAll = budgetObjectStore.getAll();
 
     getAll.onsuccess = function() {
