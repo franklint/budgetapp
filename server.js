c288069cc2567d71641d3a4454751c702053d00b
franklinT <franklintsung3@gmail.com>
2022-07-16 10:41:25 -0400

    final

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/budgetapp";
 
 const app = express();
 
@@ -23,6 +23,8 @@ mongoose.connect(MONGODB_URI, {
 
 // routes
 app.use(require("./routes/api.js"));
+
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
